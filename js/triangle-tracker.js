aa5156cd8627579a7e5d856572a948c5a59dc463
katie fujihara and Maggie Summers <maggie@>
2018-03-13 14:06:55 -0700

    add passing tests

--- js/triangle-tracker.js
+++ js/triangle-tracker.js
@@ -4,9 +4,17 @@ export function Triangle(side1, side2, side3) {
   this.side3 = side3;
 }
 
-  Triangle.prototype.checkType = function() {
-    if ((this.side1 < (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-      return "not a triangle";
+Triangle.prototype.checkType = function() {
+    if (this.side1 + this.side2 <= this.side3 || this.side2 + this.side3 <= this.side1 || this.side1 + this.side3 <= this.side2) {
+    return "not a triangle";
     }
-    console.log("conditional not reached.");
+    else if (this.side1 === this.side2 && this.side2 === this.side3) {
+    return "this is an equilateral triangle!";
+    }
+    else if (this.side1 === this.side2 || this.side2 === this.side3 || this.side3 === this.side1){
+    return "this is an isosceles triangle!";
+    }
+    else if (this.side1 != this.side2 || this.side2 != this.side3|| this.side3 != this.side1){
+    return "this is a scalene triangle!";
   }
+};
--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -9,4 +9,23 @@ describe('Triangle', function() {
     expect(triangle.side3).not.toEqual(6)
   });
 
+  it('should correctly determine whether three lengths can be made into a triangle', function () {
+    var notTriangle = new Triangle(2,3,10)
+    expect(notTriangle.checkType()).toEqual("not a triangle")
+  });
+
+ it('should correctly determine whether three lengths are equal and make an equilateral triangle', function() {
+   var equilateral = new Triangle(5, 5, 5)
+   expect(equilateral.checkType()).toEqual("this is an equilateral triangle!")
+ });
+
+ it('should correctly determine whether two lengths are equal and make an isosceles triangle', function (){
+   var isosceles = new Triangle (4, 4, 6)
+   expect(isosceles.checkType()).toEqual("this is an isosceles triangle!")
+ });
+
+ it('should correctly determine whether all three lengths are different and a scalene triangle', function (){
+   var scalene= new Triangle (4, 5, 7)
+   expect(scalene.checkType()).toEqual("this is a scalene triangle!")
+ });
 });
